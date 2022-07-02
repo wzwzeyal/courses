21c9a95ab741d4787e42695f5d2da1292387f9dd
eyalshw <wzwzeyal@gmail.com>
2022-07-02 22:37:49 +0300

    initial

--- Technion_NLP/HW/HW_02_Class/main.py
+++ Technion_NLP/HW/HW_02_Class/main.py
@@ -19,6 +19,27 @@ import pandas as pd
 import tqdm
 
 from preloading import load_datasets
+import torch
+import torch.nn as nn
+
+
+class RNN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(RNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
+        self.i2o = nn.Linear(input_size + hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, input, hidden):
+        input_combined = torch.cat((input, hidden), 1)
+        hidden = self.i2h(input_combined)
+        output = self.i2o(input_combined)
+        output = self.softmax(output)
+        return output, hidden
+
+    def initHidden(self):
+        return torch.zeros(1, self.hidden_size)
 
 
 def main():
@@ -29,24 +50,33 @@ def main():
     df = load_datasets()
 
     df.to_csv('dataset.csv')
+
+    for iter in range(1, n_iters + 1):
+        category, line, category_tensor, line_tensor = randomTrainingExample()
+        train()
     print(len(df))
 
-    #
-    #
-    #
-    # for n in range(0x90, 0xAB):
-    #     c = str(hex(n))[2:].upper()
-    #     print(c)
-
-    # for a in soup.find_all('a', href=True):
-    #     print(a)
-
-    # print(titles[4].text)
+learning_rate = 0.005 # If you set this too high, it might explode. If too low, it might not learn
 
 
+def train(category_tensor, line_tensor):
+    criterion = nn.NLLLoss()
+    rnn = RNN(10, 11, 12)
+    hidden = rnn.initHidden()
 
+    rnn.zero_grad()
 
+    for i in range(line_tensor.size()[0]):
+        output, hidden = rnn(line_tensor[i], hidden)
 
+    loss = criterion(output, category_tensor)
+    loss.backward()
+
+    # Add parameters' gradients to their values, multiplied by learning rate
+    for p in rnn.parameters():
+        p.data.add_(p.grad.data, alpha=-learning_rate)
+
+    return output, loss.item()
 
 
 if __name__ == '__main__':
@@ -59,5 +89,3 @@ if __name__ == '__main__':
     # params = vars(merge_parameter(get_params(), tuner_params))
     # main(params)
     main()
-
-
---
+++ Technion_NLP/HW/HW_02_Class/utils.py
@@ -0,0 +1,25 @@
+import torch
+import unicodedata
+import string
+
+all_letters = string.ascii_letters + " .,;'"
+
+n_letters = len(all_letters)
+
+# Find letter index from all_letters, e.g. "a" = 0
+def letterToIndex(letter):
+    return all_letters.find(letter)
+
+# Just for demonstration, turn a letter into a <1 x n_letters> Tensor
+def letterToTensor(letter):
+    tensor = torch.zeros(1, n_letters)
+    tensor[0][letterToIndex(letter)] = 1
+    return tensor
+
+# Turn a line into a <line_length x 1 x n_letters>,
+# or an array of one-hot letter vectors
+def lineToTensor(line):
+    tensor = torch.zeros(len(line), 1, n_letters)
+    for i, letter in enumerate(line):
+      tensor[i][0][letterToIndex(letter)] = 1
+    return tensor
